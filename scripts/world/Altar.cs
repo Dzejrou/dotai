ec883327c2d964c8d665c185b759e2580888666d
Jaroslav Jindrak <dzejrou@gmail.com>
2026-04-02 12:42:44 +0200

    Add altar world object

---
+++ scripts/world/Altar.cs
@@ -0,0 +1,51 @@
+using Godot;
+
+[GlobalClass]
+public partial class Altar : WorldObject
+{
+    private const string DefaultVisualDirection = "south";
+
+    [Export(PropertyHint.Enum, "east,south-east,south,south-west,west,north-west,north,north-east")]
+    public string VisualDirection { get; set; } = DefaultVisualDirection;
+
+    [Export] public Texture2D EastTexture { get; set; }
+    [Export] public Texture2D SouthEastTexture { get; set; }
+    [Export] public Texture2D SouthTexture { get; set; }
+    [Export] public Texture2D SouthWestTexture { get; set; }
+    [Export] public Texture2D WestTexture { get; set; }
+    [Export] public Texture2D NorthWestTexture { get; set; }
+    [Export] public Texture2D NorthTexture { get; set; }
+    [Export] public Texture2D NorthEastTexture { get; set; }
+
+    public override void _Ready()
+    {
+        InitializeWorldObject();
+        ApplyVisualState();
+    }
+
+    private void ApplyVisualState()
+    {
+        if (VisualSprite == null)
+            return;
+
+        VisualSprite.Texture = ResolveVisualTexture();
+        VisualSprite.Modulate = Colors.White;
+    }
+
+    private Texture2D ResolveVisualTexture()
+    {
+        var requestedDirection = string.IsNullOrWhiteSpace(VisualDirection) ? DefaultVisualDirection : VisualDirection;
+        return requestedDirection switch
+        {
+            "east" => EastTexture,
+            "south-east" => SouthEastTexture,
+            "south" => SouthTexture,
+            "south-west" => SouthWestTexture,
+            "west" => WestTexture,
+            "north-west" => NorthWestTexture,
+            "north" => NorthTexture,
+            "north-east" => NorthEastTexture,
+            _ => SouthTexture,
+        };
+    }
+}
