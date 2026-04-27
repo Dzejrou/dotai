e41d351566eb6e86db05329fd6b64bef249cd5a5
Jaroslav Jindrak <dzejrou@gmail.com>
2026-04-27 22:56:43 +0200

    Remove floating number helper

--- scripts/shared/FloatingNumberHelper.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public static class FloatingNumberHelper
-{
-    public static void ShowFloatingNumber(Node2D owner, string text, Color color, float riseDistance = 18.0f, float duration = 0.6f, int fontSize = 20)
-    {
-        FloatingText.Show(text, owner, color, riseDistance: riseDistance, duration: duration, fontSize: fontSize);
-    }
-}
